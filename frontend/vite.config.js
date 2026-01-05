7929ca0f6fba6bd9b63ecdbef1c8e854e0026e98
zaidmtz <z.mtz2000@gmail.com>
2026-01-05 09:14:32 -0600

    release: v1.2.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,16 @@
 export default {
   server: {
     host: "0.0.0.0",
-    port: process.env.PORT || 4173,
-    proxy: {
-      "/api": {
-        target: "http://spine-concepts.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    port: 5173,
   },
+
   preview: {
     host: "0.0.0.0",
     port: process.env.PORT || 4173,
-    allowedHosts: ["spine-concepts.com", "www.spine-concepts.com"],
+    allowedHosts: [
+      "spine-concepts.com",
+      "www.spine-concepts.com",
+      "spine-concepts.onrender.com",
+    ],
   },
 };
